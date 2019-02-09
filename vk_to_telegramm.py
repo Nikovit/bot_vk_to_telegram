06297377ed1aab3121b29633f934330d6f47fe5d
Sergej Gureev <bemyak@gmail.com>
2019-02-09 23:25:31 +0300

    Move max message lenth to constant

--- vk_to_telegramm.py
+++ vk_to_telegramm.py
@@ -20,6 +20,7 @@ CHANNEL = config.get('Telegram', 'CHANNEL')
 
 # Символы, на которых можно разбить сообщение
 message_breakers = [":", " ", "\n"]
+max_message_length = 4091
 
 # Инициализируем телеграмм бота
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -115,9 +116,9 @@ def send_posts_text(text):
 
 
 def split(text):
-    if len(text) >= 4096:
+    if len(text) >= max_message_length:
         last_index = max(
-            map(lambda separator: text.rfind(" ", 0, 4096), message_breakers))
+            map(lambda separator: text.rfind(separator, 0, max_message_length), message_breakers))
         good_part = text[:last_index]
         bad_part = text[last_index + 1:]
         return [good_part] + split(bad_part)
