f17ec99b2e99fb32d7614fefef3c03986075008d
Eugene Horohorin <genixoid@gmail.com>
2021-10-13 19:56:02 +0400

    debug mode, config file, improvements

--- vk_to_telegramm.py
+++ vk_to_telegramm.py
@@ -8,10 +8,10 @@ import configparser
 import logging
 from telebot.types import InputMediaPhoto
 import time
-import getopt,sys
+import getopt
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "config="])
+    opts, args = getopt.getopt(sys.argv[1:], "hc:t:d", ["help", "config=", "target=", "debug"])
 except getopt.GetoptError as err:
     # print help information and exit:
     print(str(err))  # will print something like "option -a not recognized"
@@ -19,10 +19,20 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 CONFIG_FILE="settings.ini"
+CHANNEL=""
+DEBUG_MODE=False
 
 for o, a in opts:
     if o in ("-c", "--config"):
         CONFIG_FILE = a
+    elif o in ("-t", "--target"):
+        CHANNEL = a
+    elif o in ("-h", "--help"):
+        print(os.path.basename(__file__), "-h [ -t @targetchanel ][ -c <configfile>]")
+        sys.exit()
+    elif o in ("-d", "--debug"):
+        print("Debug mode is enabled. Ini files are not updated!")
+        DEBUG_MODE=True
     else:
         assert False, "unhandled option"
 
@@ -36,9 +46,11 @@ DOMAIN = config.get('VK', 'DOMAIN')
 COUNT = config.get('VK', 'COUNT')
 VK_TOKEN = config.get('VK', 'TOKEN', fallback=None)
 BOT_TOKEN = config.get('Telegram', 'BOT_TOKEN')
-CHANNEL = config.get('Telegram', 'CHANNEL')
+if CHANNEL == "":
+    CHANNEL = config.get('Telegram', 'CHANNEL')
 INCLUDE_LINK = config.getboolean('Settings', 'INCLUDE_LINK')
 PREVIEW_LINK = config.getboolean('Settings', 'PREVIEW_LINK')
+
 
 DELIMETER = None
 if 'DELIMETER' in config['Telegram']:
@@ -47,10 +59,7 @@ if 'DELIMETER' in config['Telegram']:
 # Символы, на которых можно разбить сообщение
 message_breakers = [':', ' ', '\n']
 max_message_length = 4091
-max_capt_length = 1024
 
-delim = '\n***********************************'
-rep_delim = '\n------конец репоста-----------------'
 # Инициализируем телеграмм бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -74,8 +83,9 @@ def get_data(domain_vk, count_vk):
     if VK_TOKEN != new_token:
         VK_TOKEN = new_token
         config.set('VK', 'TOKEN', new_token)
-        with open(config_path, "w") as config_file:
-            config.write(config_file)
+        if not DEBUG_MODE:
+            with open(config_path, "w") as config_file:
+                config.write(config_file)
 
     vk = vk_session.get_api()
     # Используем метод wall.get из документации по API vk.com
@@ -141,23 +151,38 @@ def check_posts_vk():
                 str(post['owner_id']) + '_' + str(post['id'])
             links.insert(0, post_url)
         text = '\n'.join([text] + links)
-        data = {'type':'main', 'images': images, 'text': text}
-        post_data(**data)
+        send_posts_text(text)
 
+        if len(images) > 0:
+            image_urls = list(map(lambda img: max(
+                img["sizes"], key=lambda size: size["type"])["url"].replace("&type=album",""), images))
+            print(image_urls)
+            bot.send_media_group(CHANNEL, map(
+                lambda url: InputMediaPhoto(url), image_urls))
 
         # Проверяем есть ли репост другой записи
-        att_imgs = []
-        lnk_text = []
         if 'copy_history' in post:
             copy_history = post['copy_history']
             copy_history = copy_history[0]
             print('--copy_history--')
             print(copy_history)
+            text = copy_history['text']
+            send_posts_text(text)
 
             # Проверяем есть ли у репоста прикрепленное сообщение
             if 'attachments' in copy_history:
                 copy_add = copy_history['attachments']
                 copy_add = copy_add[0]
+
+                # Если это ссылка
+                if copy_add['type'] == 'link':
+                    link = copy_add['link']
+                    text = link['title']
+                    send_posts_text(text)
+                    img = link['photo']
+                    send_posts_img(img)
+                    url = link['url']
+                    send_posts_text(url)
 
                 # Если это картинки
                 if copy_add['type'] == 'photo':
@@ -165,27 +190,13 @@ def check_posts_vk():
                     for img in attach:
                         if 'photo' in img:
                             image = img['photo']
-                            att_imgs.append(image)
-
-                # Если это ссылка
-                if copy_add['type'] == 'link':
-                    link = copy_add['link']
-                    lnk_text.append(link['title'])
-                    lnk_text.append(link['url'])
-                    #TODO: need refactor this -when we have multiple images and this in same time
-                    img = link['photo']
-                    #TBD: need to figure out this
-                    #send_posts_img(img)
-                    att_imgs.append(img)
-
-            text = '\n'.join([copy_history['text']] + lnk_text)
-            data = {'type':'repost', 'images': att_imgs, 'text': text}
-            post_data(**data)
+                            send_posts_img(image)
 
         # Записываем id в файл
         config.set('Settings', 'LAST_ID', str(post['id']))
-        with open(config_path, "w") as config_file:
-            config.write(config_file)
+        if not DEBUG_MODE:
+            with open(config_path, "w") as config_file:
+                config.write(config_file)
 
         if DELIMETER is not None:
             send_posts_text(DELIMETER)
@@ -195,90 +206,41 @@ def check_posts_vk():
 # Отправляем посты в телеграмм
 
 
-def post_data(**data):
-    # data - {'type':type,'images':images, 'text':text}
-    type = data.get('type', 'main')
-    images = data.get('images',[])
-    text = data.get('text','')
-    body = {'type': type, 'pic': False, 'body': text }
-        if len(images) > 1:
-            image_urls = list(map(lambda img: max(
-                img["sizes"], key=lambda size: size["type"])["url"], images))
-            print(image_urls)
-            bot.send_media_group(CHANNEL, map(
-                lambda url: InputMediaPhoto(url), image_urls))
-            send_posts_text(**body)
-        elif len(images) == 1:
-            body['pic'] = True
-            send_posts_img(**body)
-        else:
-            send_posts_text(**body)
-
 # Текст
-def send_posts_text( **data):
+def send_posts_text(text):
     global CHANNEL
     global PREVIEW_LINK
     global bot
 
-    type = data.get('type', 'main')
-    text = data.get('text')
-
-    # В телеграмме есть ограничения на длину одного сообщения в 4091 символ, разбиваем длинные сообщения на части
-    for msg in split(**data):
-        bot.send_message(CHANNEL, msg, disable_web_page_preview=not PREVIEW_LINK)
-
-def set_delim(type):
-    global delim
-    global rep_delim
-
-    if type == 'main':
-        return delim
-    elif type == 'repost':
-        return rep_delim
+    if text == '':
+        print('no text')
     else:
-        return None
+        # В телеграмме есть ограничения на длину одного сообщения в 4091 символ, разбиваем длинные сообщения на части
+        for msg in split(text):
+            bot.send_message(CHANNEL, msg, disable_web_page_preview=not PREVIEW_LINK)
 
 
-def split(**message):
-    # message format: {'type': 'main', 'pic':True, 'body': 'текст сообщения'}
-    # message types: main - 'common text for messages and caption', repost - 'text or caption for repost'
-    # message def: 'pic' True or False, if image exists
-
+def split(text):
     global message_breakers
     global max_message_length
-    global max_capt_length
 
-    type = message.get('type', 'main')
-    text = message.get('body')
-
-    if message.get('pic', False):
-        max_length = max_capt_length
-    else:
-        max_length = max_message_length
-
-    delim = set_delim(type)
-    text = text + delim
-
-    if len(text) >= max_length:
+    if len(text) >= max_message_length:
         last_index = max(
             map(lambda separator: text.rfind(separator, 0, max_message_length), message_breakers))
         good_part = text[:last_index]
-        message['body'] = text[last_index + 1:]
-        return [good_part] + split(**message)
+        bad_part = text[last_index + 1:]
+        return [good_part] + split(bad_part)
     else:
         return [text]
 
 
 # Изображения
-def send_posts_img(**object):
+def send_posts_img(img):
     global bot
-
-    img = object.get('images', [])[0]
+    
     # Находим картинку с максимальным качеством
     url = max(img["sizes"], key=lambda size: size["type"])["url"]
-
-    caption = split(**object)[0]
-    bot.send_photo(CHANNEL, url, caption)
+    bot.send_photo(CHANNEL, url)
 
 
 if __name__ == '__main__':
